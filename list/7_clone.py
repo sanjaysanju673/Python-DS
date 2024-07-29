b3fe19a8b8eab712bec8126f14a9395796280f36
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-07-29 13:35:53 +0530

    [sanjay] Add:program to clone or copy the  list

---
+++ list/7_clone.py
@@ -0,0 +1,40 @@
+'''
+@Author: v sanjay kumar
+@Date: 2024-07-24 05:30:30
+@Last Modified by: v sanjay kumar
+@Last Modified time: 2024-07-24 05:30:30.
+@Title : Python program to clone or copy the  list
+'''
+
+
+def duplicate_list(sam_list):
+    """
+    descripton
+       clone the given list.
+'''
+
+    Parameters:
+    - it_list(list):  given list.
+
+    Returns:
+    - list1(list).-the final list cloned from the given list.
+    
+    """
+    
+    res=sam_list 
+   
+    return res
+        
+
+
+
+    
+def main():
+    sam_list =[1, 3, 5, 6, 3, 5, 6, 1]
+    print("first given list is :",sam_list)
+    print("The cloned  list is : " ,duplicate_list(sam_list))
+
+
+
+if __name__ =="__main__":
+    main()
