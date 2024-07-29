e6d6423b0df9714f4440d1077f6c5806aa45e80e
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-07-29 15:09:04 +0530

    [Sanjay] Add:program to create tuple.

---
+++ tuple/createtuple.py
@@ -0,0 +1,35 @@
+'''
+
+    @Author: v sanjay kumar
+    @Date: 2024-07-25 2:00:30
+    @Last Modified by: v sanjay kumar
+    @Last Modified time: 2024-07-25 2:00:30
+    @Title :create tuple.
+
+'''
+
+
+def creattuple():
+    """
+    Description:
+        create tuple.
+
+    Parameters:
+        void (none): none.
+
+    Returns:
+        set: return the tuple.
+    """
+    x =(1,3,4,5,6,7,8)
+  
+    return x
+
+
+def main():
+    x=creattuple()
+    print(x)
+    print(type(x))
+
+
+if __name__ == "__main__":
+    main()
